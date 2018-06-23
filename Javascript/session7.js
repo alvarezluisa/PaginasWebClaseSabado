e1f1d526d4e293f6f1b54e410b5128bac6b2f053
alvarezluisa <alvarez.luisa@javeriana.edu.co>
2018-06-23 14:05:30 -0500

    Prueba primer repo

---
+++ Javascript/session7.js
@@ -0,0 +1,12 @@
+function cambiarstyle (){
+  document.getElementById('pMensaje').style.fontsize = "45px";
+
+}
+function sumarrestar (suma) {
+  var arreglonumero = [5,2];
+  resulta = arreglonumero [0] + arreglonumero [1];
+  alert("suma" + suma + ": resultados");
+
+}
+function c (Negacion)
+var visible= true;
