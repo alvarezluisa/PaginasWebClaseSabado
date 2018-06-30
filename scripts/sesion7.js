c12978894b93d73151c80ebf483dd23b233ef18d
alvarezluisa <alvarez.luisa@javeriana.edu.co>
2018-06-30 11:21:00 -0500

    Finalización taller clase 7

--- scripts/sesion7.js
+++ scripts/sesion7.js
@@ -24,39 +24,112 @@ function sumaYResta(){
   alert(resultado);
 }
 
-function negacion(){
-  var visible = true;
-  alert(!visible);  // Muestra "false" y no "true"
-  var cantidad = 0; // 0 es falso 1 true
-  var resultado = !cantidad;  // resultado = true
-  alert(resultado);
-  cantidad = 2;
-  resultado = !cantidad;  // resultado = false
-  alert(resultado);
-  var mensaje = "";
-  resultado = !mensaje;  // resultado = true
-  alert(resultado);
+var operadoresLogicos={
+  negacion:function(){
+    var visible = true;
+    alert(!visible);  // Muestra "false" y no "true"
+    var cantidad = 0; // 0 es falso 1 true
+    var resultado = !cantidad;  // resultado = true
+    alert(resultado);
+    cantidad = 2;
+    resultado = !cantidad;  // resultado = false
+    alert(resultado);
+    var mensaje = "";
+    resultado = !mensaje;  // resultado = true
+    alert(resultado);
+  },
+
+  operadorAND:function(){
+    var valor1 = true;
+    var valor2 = false;
+    resultado = valor1 && valor2; // resultado = false
+    alert(resultado);
+    valor1 = true;
+    valor2 = true;
+    resultado = valor1 && valor2; // resultado = true
+    alert(resultado);
+  },
+
+  operadorOR:function(){
+    var valor1 = true;
+    var valor2 = false;
+    resultado = valor1 || valor2; // resultado = true
+     alert(resultado);
+    valor1 = false;
+    valor2 = false;
+    resultado = valor1 || valor2; // resultado = false
+    alert(resultado);
+  }
+
+};
+
+function operadoresMatematicos (suma1, suma2, resta, multiplicacion,division){
+  var sumar = suma1 + suma2;
+  var restar =sumar- resta;
+  var multiplicar = sumar * multiplicacion;
+  var dividir = sumar/division;
+  var operacionModulo = sumar %division;
+  alert("SUMA " + sumar + " Resta " + restar + " multiplicacion " + multiplicar + " dividir " + dividir + " muodulo " + operacionModulo);
+
+}
+function relaciones (numero1,numero2,cadena1,cadena2){
+  var mayorQue = numero1 > numero2;
+  var menorQUE = numero1 < numero2;
+
+  var mayoroIgual = numero1 >= numero2;
+  var menorroIgual = numero1 <= numero2;
+  var validaIgual = numero1 == numero2;
+  var validasiDiferente = numero1 != numero2;
+  alert("Valida dos numeros " + numero1 + "  " + numero2 + "\n mayorQue "
+        + mayorQue + " menorQUE " + menorQUE + " mayoroIgual " + mayoroIgual
+       + " validaIgual " + validaIgual + " validasiDiferente " + validasiDiferente);
+  relacionesCadenas(cadena1,cadena2);
 }
 
-function  operadorAND(){
-  var valor1 = true;
-  var valor2 = false;
-  resultado = valor1 && valor2; // resultado = false
-  alert(resultado);
-  valor1 = true;
-  valor2 = true;
-  resultado = valor1 && valor2; // resultado = true
-  alert(resultado);
+//FUncion encargada de realizar comprobaciones de realcionales sobre cadenas
+//cadena1 String a comparar
+//cadena2 String a comparar
+function relacionesCadenas(cadena1, cadena2) {
+  var compararCadena = cadena1 == cadena2;
+  var diferente = cadena1 != cadena2;
+  var mayor = cadena1 >= cadena2;
+  alert("Igual " + compararCadena + " diferente  " + diferente + "\n mayor "
+        + mayor );
 }
 
-function operadorOR(){
-  var valor1 = true;
-  var valor2 = false;
-  resultado = valor1 || valor2; // resultado = true
-   alert(resultado);
-  valor1 = false;
-  valor2 = false;
-  resultado = valor1 || valor2; // resultado = false
-  alert(resultado);
+function mayorEdad(){
+  var edad =   document.getElementById('txtTexto').value;
+  if(edad >=18){
+     alert("Eres mayor de edad" );
+   }
+  else{
+    alert("No Eres mayor de edad" );
+  }
+}
 
+function clasificarEDAD(){
+  var edad = document.getElementById('txtTexto').value;
+  if(edad < 12) {
+    alert("Todavía eres muy pequeño");
+  }
+  else if(edad < 19) {
+    alert("Eres un adolescente");
+  }
+  else if(edad < 35) {
+    alert("Aun sigues siendo joven");
+  }
+  else {
+    alert("Piensa en cuidarte un poco más");
+  }
+
+}
+
+function potenciacion(){
+  var base =   document.getElementById('txtBase').value;
+  var exponete =   document.getElementById('txtExponente').value;
+  var resultado = base;
+  for(var i = 1; i < exponete; i++) {
+      resultado = resultado * base;
+  }
+  document.getElementById('txtResultado').value = resultado;
 }
